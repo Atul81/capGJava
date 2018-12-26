668d21a20a6182b452280be89ccfed70dfae70ad
Atul81 <atulkp.eee13@nituk.ac.in>
2018-12-26 23:26:19 +0530

    atul|Orika Classes

---
+++ src/main/java/orikaModel/Dest.java
@@ -0,0 +1,42 @@
+package orikaModel;
+
+import java.util.Date;
+
+public class Dest {
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	private String[][] aliases;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public String[][] getAliases() {
+		return aliases;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public void setAliases(String[][] aliases) {
+		this.aliases = aliases;
+	}
+}
